db0bf65288646b09ee5af5e8c75c7b2810a77226
datispham <datistpham@gmail.com>
2022-08-14 06:27:21 +0700

    final

--- pages/components/Contact/ContactPhonenumber.js
+++ pages/components/Contact/ContactPhonenumber.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContactPhonenumber = (props) => {
   return (
-    <div onClick={()=> window.location.href= "tel://0388015984"} id="wrapper" >
+    <div onClick={()=> window.location.href= "tel://0962839883"} id="wrapper" >
       <div className="ring" style={{width: "100%", height: "100%"}}>
         <div className="coccoc-alo-phone coccoc-alo-green coccoc-alo-show">
           <div className="coccoc-alo-ph-circle"></div>
